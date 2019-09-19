4274955afbe6015a2ce0a9acd82810eeba9c9f65
zengjielin <248717607@qq.com>
2019-09-19 14:06:09 +0800

    更新日期组件

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import * as serviceWorker from './serviceWorker';
 import 'antd-mobile/dist/antd-mobile.css';
 import 'lib-flexible'
 import './assets/css/base.css'
-console.log("index")
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/views/order_admin/order_admin.js
+++ src/views/order_admin/order_admin.js
@@ -4,15 +4,35 @@ import React, {
 import { Icon } from 'antd-mobile';
 import {HashRouter, NavLink} from "react-router-dom";
 import './order_admin.scss'
-
+import { DatePicker } from 'antd-mobile';
 class OrderAdmin extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            dpValue: null,
+            dateVisible: false,
+        }
+    }
+    formatDate(date) {
+        /* eslint no-confusing-arrow: 0 */
+        const pad = n => n < 10 ? `0${n}` : n;
+        const dateStr = `${new Date(date).getFullYear()}-${pad(new Date(date).getMonth() + 1)}-${pad(new Date(date).getDate())}`;
+        const timeStr = `${pad(new Date(date).getHours())}:${pad(new Date(date).getMinutes())}`;
+        return `${dateStr}`;
+    }
+    selectDate(date){
+        this.setState({
+            dpValue :this.formatDate(date)
+        })
+        console.log(this.formatDate(date))
+    }
     render() {
         return (
             <div className="order-admin-page">
                 <div className="header-wrapper">
-                    <div className="query-date-wrapper">
+                    <div className="query-date-wrapper" onClick={() => this.setState({ dateVisible: true })}>
                         <img src={require("../../assets/img/icon_date.png")} alt="" />
-                        <span className="date-text">2019-9-17</span>
+                        <span className="date-text">{this.state.dpValue}</span>
                         <Icon type="down" color="#8e8e8e" />
                     </div>
                     <div className="today-food-entry">
@@ -63,6 +83,14 @@ class OrderAdmin extends Component {
                                             <NavLink to="/order_detail" className="detail-btn">详情</NavLink>
                                         </HashRouter>
                                     </div>
+                                    
+                                    <DatePicker
+                                        mode="date"
+                                        visible={this.state.dateVisible}
+                                        value={this.state.dpValue}
+                                        onOk={date => this.selectDate(date)}
+                                        onDismiss={() => this.setState({ dateVisible: false })}
+                                        />
                                 </div>
                             )
                         })
